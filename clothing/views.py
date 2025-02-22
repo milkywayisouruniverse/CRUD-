3e7b8de9bc4ef6ca718e699d597f2a8b163510c5
milkywayisouruniverse <dazzlingphoenix2022@gmail.com>
2025-02-22 12:24:43 +0530

    Eeshika's first commit

--- clothing/views.py
+++ clothing/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
 from clothing.models import Product
+from .forms import ProductForm
 
 # Create your views here.
 def clothing(request):
-    pro_img = Product.objects.all
-    return render(request, 'clothing/fashion.html',{'pro_img': pro_img})
+    form = ProductForm()
+    pro_img = Product.objects.all()
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            form = ProductForm()  # reset form after saving
+
+    return render(request, 'clothing/fashion.html',{'pro_img': pro_img,'form':form})
+
