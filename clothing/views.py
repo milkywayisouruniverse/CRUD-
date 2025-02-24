61a38af4249ef114ce575dead46d6ea26084e156
milkywayisouruniverse <dazzlingphoenix2022@gmail.com>
2025-02-24 14:26:19 +0530

    3rd commit

--- clothing/views.py
+++ clothing/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
 from clothing.models import Product
 from .forms import ProductForm
 
@@ -15,3 +15,30 @@ def clothing(request):
 
     return render(request, 'clothing/fashion.html',{'pro_img': pro_img,'form':form})
 
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Product
+from .forms import ProductForm
+
+def edit_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            return redirect('clothing')  # Make sure 'clothing' is a valid URL name
+
+    else:
+        form = ProductForm(instance=product)
+
+    return render(request, 'clothing/edit_product.html', {'form': form, 'product': product})
+
+def delete_product(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+
+    if request.method == "POST":
+        product.delete()
+        return redirect('clothing')  # Ensure this is a valid URL name
+
+    return render(request, 'clothing/confirm_delete.html', {'product': product})
